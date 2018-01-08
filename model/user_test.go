425216ad44b2f1dfcde2a5c13bd0f8c46d878797
spby <siwanon.turbow@gmail.com>
2018-01-08 16:48:25 +0700

    itgr test model finduser ss

--- model/user_test.go
+++ model/user_test.go
@@ -1,2 +1,24 @@
 package model_test
 
+import (
+	"testing"
+
+	"github.com/bosemian/go-webbord/model"
+)
+
+func TestFindUserByUsername(t *testing.T) {
+	db := prepareDB(t)
+	defer db.Close()
+
+	db.Exec(`insert into users (id, username, password) values (1, 'user1', 'password')`)
+
+	_, err := model.FindUserByUsername(db, "user1")
+	if err != nil {
+		t.Fatalf("FindUserByUsername expected returned nil error; got %v", err)
+	}
+	var cnt int
+	db.QueryRow(`select count(*) from users`).Scan(&cnt)
+	if cnt != 1 {
+		t.Fatalf("FindUserByUsername expected retrun 1 users; got %d", cnt)
+	}
+}
